944ab0d82adcc26567c88a9e5e571e531fc6393d
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-07-08 18:35:42 +0500

    Bajarildi

--- contact.py
+++ contact.py
@@ -1,33 +1,63 @@
-from printer import print_status, print_all_contact
+from printer import print_status,print_all_contact
 
-def add_contact(contacts: list[dict]):
-    first_name = input("first name: ")
-    last_name = input("last name: ")
-    phone = input("phone: ")
-    group = input("group (family, friend, work, other): ")
+def add_contact(contacts:list[dict]):
+    
+    first_name = input("first name : ")
+    last_name = input("last name : ")
+    phone = input("number : ").replace(" ","")
+    group = input("group (family,friends,work,other): ")
+    
+    if first_name.isalpha() and last_name.isalpha() and phone.isdigit() and group in ("family","friends","work","other" ):
+        contacts.append({
+            "first_name" : first_name,
+            "last_name"  : last_name,
+            "phone"   : phone,
+            "group"   : group,
+        })
+        print_status("success")
+    else:
+        print_status("error")
 
-    contacts.append({
-        "first_name": first_name,
-        "last_name": last_name,
-        "phone": phone,
-        "group": group,
-    })
-
-    print_status('success')
-
-def show_all_contact(contacts: list[dict]):
-
+def show_all_contact(contacts:list[dict]):
+    
     if contacts:
         print_all_contact(contacts)
     else:
-        print_status('error')
+        print("kontakt mavjud emas")
+        
 
-def search_contact():
-    pass
+def search_contact(contacts):
+    search = input("search : ").lower()
+    
+    result = list(filter(
+        lambda contact: search in contact.values(),contacts
+    ))
+    
+    if result:
+        print(result)
+    else:
+        print_status("error")
+            
+def delete_contact(contacts):
+    
+    delete = input("delete : ").lower().replace(" ","")
+    
+    for contact in contacts:
+        for value in contact.values():
+            if value.replace(" ","").lower() == delete:
+                print_status("success")
+                contacts.remove(contact)
+    else:print_status('error')     
 
-def delete_contact():
-    pass
-
-def update_contact():
-    pass
+def update_contact(contacts):
+    
+    old_value = input("mavjud malumotni kiriting ")
+    new_value = input("yangi malumotni  kiriting ")
+    
+    for contact in contacts:
+        for key,value in contact.items():
+            if old_value == value:
+                contact[key] = new_value
+        else:
+                print_status('error') 
 
--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from sys import exit
 from printer import print_menu
-from contact import add_contact, show_all_contact
-
+from contact import add_contact, show_all_contact,search_contact,delete_contact,update_contact
 
 def main():
-    contacts: list[dict] = [
-        {"first_name": 'ali', 'last_name': 'valiyev', 'phone': '3241234123', "group": 'other'}
-    ]
+    contacts: list[dict] = [{
+        'first_name':'Shohjahon',
+        'last_name':"Mardonov",
+        "phone":'94 081 01 26',
+        'group':"family"
+        }]
     
     while True:
         print_menu()
-
-        choice = input("Menu tanlang: ")
-
-        if choice == '1':
+        
+        choice = int(input("Menudan birini tanlang :"))
+        
+        if choice == 1:
             add_contact(contacts)
-        elif choice == '2':
+        
+        elif choice == 2:
             show_all_contact(contacts)
-        elif choice == '3':
-            pass
-        elif choice == '4':
-            pass
-        elif choice == '5':
-            pass
-        elif choice == '6':
-            pass
+        
+        elif choice == 3:
+            search_contact(contacts)
+        
+        elif choice == 4:
+            delete_contact(contacts)
+        
+        elif choice == 5:
+            update_contact(contacts)
+        
         else:
             exit(0)
+            
 
 if __name__ == "__main__":
     main()
